dc07a60fbe8af0dd1d70e21c94c02c482a9ab75b
Kieferer <balazsborsi0@gmail.com>
2023-05-29 20:19:31 +0200

    feat: Create Hierarchy.jsx and hierarchy.css as a resizable window

---
+++ src/elements/window/tool/Hierarchy.jsx
@@ -0,0 +1,44 @@
+import './hierarchy.css'
+import {useEffect} from "react";
+const Hierarchy = () => {
+  useEffect(() => {
+    const resizableDiv = document.getElementById('myResizableDiv');
+    let startX;
+    let startWidth;
+
+    const handleMouseDown = (event) => {
+      startX = event.clientX;
+      startWidth = parseInt(
+        document.defaultView.getComputedStyle(resizableDiv).width,
+        10
+      );
+
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('mouseup', handleMouseUp);
+    };
+
+    const handleMouseMove = (event) => {
+      const width = startWidth + (event.clientX - startX);
+      resizableDiv.style.width = `${width}px`;
+    };
+
+    const handleMouseUp = () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+
+    resizableDiv.addEventListener('mousedown', handleMouseDown);
+
+    return () => {
+      resizableDiv.removeEventListener('mousedown', handleMouseDown);
+    };
+  }, []);
+
+  return (
+    <div className={"hierarchyPanel"} id="myResizableDiv">
+      <p className={"hierarchyTitle"}>HIERARCHY</p>
+    </div>
+  )
+}
+
+export default Hierarchy;
