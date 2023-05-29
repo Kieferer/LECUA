c69ffb9f05cfea38e35c87fae0b14b840eb860d4
Kieferer <balazsborsi0@gmail.com>
2023-05-29 20:16:22 +0200

    feat: Create TitleBar.jsx and titleBar.css with navigation bar, menus and logo

---
+++ src/elements/window/title/TitleBar.jsx
@@ -0,0 +1,31 @@
+import { appWindow } from '@tauri-apps/api/window';
+import React, { useEffect } from 'react';
+import './titleBar.css';
+import Button from "../../button/Button.jsx";
+import NavigationMenu from "./navigationMenu/NavigationMenu.jsx";
+
+const TitleBar = () => {
+  useEffect(() => {
+    const titlebar = document.getElementById("titlebar");
+    titlebar.addEventListener("drag", appWindow.startDragging)
+  }, [])
+
+  return (
+    <div data-tauri-drag-region className="titlebar" id={"titlebar"}>
+      <div className={"titlebar-left"}>
+        <div className="titlebar-menu">
+          <img src="/vite.svg" className="logoApp" alt="Vite logo" />
+        </div>
+        <div className="titlebar-menu">
+            <Button label={"File"}/>
+            <Button label={"Edit"}/>
+            <Button label={"View"}/>
+        </div>
+      </div>
+      <div className={"titleName"}>LECUA</div>
+      <NavigationMenu/>
+    </div>
+  );
+};
+
+export default TitleBar;
