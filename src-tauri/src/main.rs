3b7f5e835af90928c73aaae0f3626ccc04254bc1
Kieferer <balazsborsi0@gmail.com>
2023-06-01 22:01:40 +0200

    feat: Import math module and add to builder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[path = "./utility/math.rs"]
+mod math;
 #[path = "./ui/code-editor/cursor.rs"]
 mod cursor;
 
@@ -8,7 +10,9 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             cursor::insert_cursor_symbol,
             cursor::insert_text_at_cursor,
-            cursor::get_length_of_lines
+            cursor::adjust_cursor_y,
+            cursor::get_length_of_lines,
+            math::clamp
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
