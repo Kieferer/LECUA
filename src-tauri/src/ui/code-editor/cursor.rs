6a56c1f40b111dec43a173329767e8067c79f3ea
Kieferer <balazsborsi0@gmail.com>
2023-06-01 22:03:14 +0200

    feat: Fully implement adjust_cursor_y. Import math module.

--- src-tauri/src/ui/code-editor/cursor.rs
+++ src-tauri/src/ui/code-editor/cursor.rs
@@ -1,7 +1,8 @@
 use std::cmp;
+use crate::math::clamp;
 
 #[tauri::command]
-pub fn insert_cursor_symbol(mut text: String, pos: usize,) -> String {
+pub fn insert_cursor_symbol(mut text: String, pos: usize) -> String {
     text.insert(cmp::min(text.len(), pos), '┃');
     text
 }
@@ -24,9 +25,18 @@ pub fn get_length_of_lines(text: String, y: usize) -> usize {
 
     y_length
 }
+
 #[tauri::command]
-pub fn adjust_cursor_y(text: String, y: usize, direction: i8) -> String {
-    get_lines(&text, y).join("")
+pub fn adjust_cursor_y(text: String, mut y: usize, direction: i8) -> usize {
+    let length_of_rows: usize = get_lines(&text, y).len();
+
+    if direction > 0 && length_of_rows >= (y + 1) {
+        y += 1;
+    } else if direction < 0 && y > 0 {
+        y -= 1;
+    }
+
+    y
 }
 
 fn get_lines(text: &str, y: usize) -> Vec<&str> {
