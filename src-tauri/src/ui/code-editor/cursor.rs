be37ed7b428ec3bab9931f232fd3cb8bd62cad1e
Kieferer <balazsborsi0@gmail.com>
2023-06-03 02:07:07 +0200

    feat: Create adjust_cursor_x method

--- src-tauri/src/ui/code-editor/cursor.rs
+++ src-tauri/src/ui/code-editor/cursor.rs
@@ -20,7 +20,7 @@ pub fn insert_text_at_cursor(mut text: String, inserted_text: String, x: usize, 
 
 #[tauri::command]
 pub fn get_length_of_lines(text: String, y: usize) -> usize {
-    let rows_till_y: Vec<&str> = get_lines(&text, y);
+    let rows_till_y: Vec<&str> = text.lines().take(y).collect();
     let y_length = rows_till_y.join("").len();
 
     y_length
@@ -28,18 +28,33 @@ pub fn get_length_of_lines(text: String, y: usize) -> usize {
 
 #[tauri::command]
 pub fn adjust_cursor_y(text: String, mut y: usize, direction: i8) -> usize {
-    let length_of_rows: usize = get_lines(&text, y).len();
+    let length_of_rows: usize = text.lines().collect::<Vec<&str>>().len();
 
-    if direction > 0 && length_of_rows >= (y + 1) {
+    if direction > 0 && (length_of_rows - 1) > y {
         y += 1;
-    } else if direction < 0 && y > 0 {
+    } else if direction < 0 && (y > 0) {
         y -= 1;
     }
 
     y
 }
 
-fn get_lines(text: &str, y: usize) -> Vec<&str> {
-    let amount: usize = y + 1;
-    text.lines().take(amount).collect()
+#[tauri::command]
+pub fn adjust_cursor_x(text: String, mut x: usize, mut y: usize, direction: i8) -> (usize, usize) {
+    let current_row: Option<&str> = text.lines().take(y).collect::<Vec<&str>>().get(0).cloned();
+
+    if let Some(row) = current_row {
+        if direction > 0 {
+            if  current_row.unwrap().len() >= (x + 1) {
+                x += 1;
+            } else {
+                x = 0;
+                y += 1;
+            }
+        } else if direction < 0 {
+            x -= 1;
+        }
+    }
+
+    (x , y)
 }
