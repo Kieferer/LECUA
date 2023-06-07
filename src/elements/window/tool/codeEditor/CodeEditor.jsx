c598232159f7a15627867db0a47f5cffd2a872fd
Kieferer <balazsborsi0@gmail.com>
2023-06-07 03:01:14 +0200

    fix: Commenting out badly implemented methods to avoid crash

--- src/elements/window/tool/codeEditor/CodeEditor.jsx
+++ src/elements/window/tool/codeEditor/CodeEditor.jsx
@@ -7,8 +7,9 @@ import './codeEditor.css'
 
 
 const CodeEditor = () => {
+  const [cursorPos, setCursorPos] = useState(0);
   const [cursorPosX, setCursorPosX] = useState(0);
-  const [cursorPosY, setCursorPosY] = useState(1);
+  const [cursorPosY, setCursorPosY] = useState(0);
   const [code, setCode] = useState('');
   const [visualizerCode, setVisualizerCode] = useState('')
   const editorRef = useRef(null);
@@ -16,55 +17,61 @@ const CodeEditor = () => {
   const handleKeyDown = (event) => {
     if (event.target.id === editorRef.current.id) {
       if (event.key === "ArrowRight") {
-        setCursorPosX((value) => Math.min(code.length, value + 1));
+        setCursorPos((value) => Math.min(code.length, value + 1));
+        /*invoke("adjust_cursor_x", {text: code, x: cursorPosX, y: cursorPosY, direction: 1}).then(([x, y]) => {
+          setCursorPosX(x);
+          setCursorPosY(y);
+        });*/
       }
       if (event.key === "ArrowLeft") {
-        setCursorPosX((value) => Math.max(0, value - 1));
+        setCursorPos((value) => Math.max(0, value - 1));
+        //invoke("adjust_cursor_x", {text: code, x: cursorPosX, y: cursorPosY, direction: -1})
       }
       if (event.key === "ArrowUp") {
-        setCursorPosY((value) => Math.max(1, value - 1));
-        invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPosX((value) => value - y_length));
+        //invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: -1}).then((value) => setCursorPosY(value));
+        //invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPos((value) => value - y_length));
       }
       if (event.key === "ArrowDown") {
-        setCursorPosY((value) => Math.min(code.length, value + 1));
-        invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPosX((value) => value + y_length));
+        //invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: 1}).then((value) => setCursorPosY(value));
+        //invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPos((value) => value + y_length));
       }
       if (event.key === "Backspace") {
         setCode((prevText) => prevText.slice(0, -1));
-        setCursorPosX((value) => Math.max(0, value - 1));
+        setCursorPos((value) => Math.max(0, value - 1));
         setCursorPosY((value) => Math.min(code.length, value + 1));
         return;
       }
 
       if (event.key === "Enter") {
         setCode((prevText) => prevText + "\n");
-        setCursorPosX((value) => value + 1);
+        setCursorPos((value) => value + 1);
       }
 
       if (event.ctrlKey) {
+        invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: 0}).then(m => console.log(m))
         return;
       }
       if (event.key.length < 2) {
         setCode((prevText) => prevText + event.key);
-        setCursorPosX((value) => value + 1);
+        setCursorPos((value) => value + 1);
       }
     }
   };
 
   useEffect(() => {
-    invoke('insert_cursor_symbol', {text: code, pos: Math.min(code.length, cursorPosX)}).then((message => {
+    invoke('insert_cursor_symbol', {text: code, pos: Math.min(code.length, cursorPos)}).then((message => {
       setVisualizerCode(message);
     }));
-    setCursorPosX((value) => Math.min(code.length, value));
+    setCursorPos((value) => Math.min(code.length, value));
     setTimeout(Prism.highlightAll, 1);
-  }, [visualizerCode, cursorPosX, cursorPosY]);
+  }, [visualizerCode, cursorPos]);
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [code]);
+  }, [code, cursorPosY]);
 
   return (
     <div className={"editorPanel"}>
