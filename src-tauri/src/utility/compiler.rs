aa36035f1bbf136df76ff6cd3f605f4959c6cc6a
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-06-19 19:23:43 +0200

    feat: Add compile status, compile stderr, compile stdout and run output

--- src-tauri/src/utility/compiler.rs
+++ src-tauri/src/utility/compiler.rs
@@ -1,3 +1,4 @@
+use std::env::temp_dir;
 use std::io::Write;
 use tempfile::NamedTempFile;
 use std::process::{Command, Stdio};
@@ -11,11 +12,32 @@ pub fn compile(code: String) {
         .expect("Failed to write to file");
 
     let file_path = file.path().to_str().expect("Invalid file path");
+    let directory_path = file.path().parent().unwrap().to_str().unwrap().to_owned();
+    println!("ez: {:?}", directory_path);
+
 
     let output = Command::new("rustc")
         .arg("--crate-name")
-        .arg("compile_crate")
+        .arg("filename")
         .arg(file_path)
+        .arg("--out-dir " + &directory_path)
         .output()
         .expect("Failed to execute command");
+
+
+    if !output.stdout.is_empty() {
+        let out_string = std::str::from_utf8(&output.stdout).expect("Failed to convert output to string");
+        println!("Output: {}", out_string);
+    }
+
+    if !output.stderr.is_empty() {
+        let error_string = std::str::from_utf8(&output.stderr).expect("Failed to convert error output to string");
+        println!("Error: {}", error_string);
+    } else {
+        let run_output = Command::new(&directory_path + "/filename.exe")
+            .output();
+        println!("Run: {:?}", run_output);
+
+    }
+    println!("status: {}", output.status);
 }
