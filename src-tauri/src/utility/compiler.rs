37afad2da3453adf0985f6d1c7f3016cc87ac2f3
Kieferer <balazsborsi0@gmail.com>
2023-06-29 15:28:32 +0200

    feat: Added running the project code after successfully compiled

--- src-tauri/src/utility/compiler.rs
+++ src-tauri/src/utility/compiler.rs
@@ -1,8 +1,8 @@
+use std::any::Any;
 use std::env::temp_dir;
 use std::io::Write;
 use tempfile::NamedTempFile;
-use std::process::{Command, Stdio};
-
+use std::process::{Command, ExitStatus, Stdio};
 
 #[tauri::command]
 pub fn compile(code: String) {
@@ -13,31 +13,38 @@ pub fn compile(code: String) {
 
     let file_path = file.path().to_str().expect("Invalid file path");
     let directory_path = file.path().parent().unwrap().to_str().unwrap().to_owned();
-    println!("ez: {:?}", directory_path);
-
 
     let output = Command::new("rustc")
         .arg("--crate-name")
         .arg("filename")
         .arg(file_path)
-        .arg("--out-dir " + &directory_path)
+        .arg("--out-dir")
+        .arg(directory_path.clone())
         .output()
         .expect("Failed to execute command");
 
-
-    if !output.stdout.is_empty() {
-        let out_string = std::str::from_utf8(&output.stdout).expect("Failed to convert output to string");
-        println!("Output: {}", out_string);
-    }
-
-    if !output.stderr.is_empty() {
+    if !output.status.success() {
         let error_string = std::str::from_utf8(&output.stderr).expect("Failed to convert error output to string");
-        println!("Error: {}", error_string);
+        println!("Compile error: {}", error_string);
     } else {
-        let run_output = Command::new(&directory_path + "/filename.exe")
+        let run_output = Command::new(directory_path + "/filename.exe")
             .output();
-        println!("Run: {:?}", run_output);
 
+        match run_output {
+            Ok(output) => {
+                if !output.status.success() {
+                    let error_string = std::str::from_utf8(&output.stderr).expect("Failed to convert error output to string");
+                    println!("Run error: {}", error_string);
+                } else {
+                    let output_string = std::str::from_utf8(&output.stdout).expect("Failed to convert output to string");
+                    println!("{}", output_string);
+                }
+            }
+            Err(err) => {
+                println!("Failed to execute run command: {}", err);
+            }
+        }
     }
+
     println!("status: {}", output.status);
 }
