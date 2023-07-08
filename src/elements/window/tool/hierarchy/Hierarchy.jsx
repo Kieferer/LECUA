2282664eb23d904884d1808d5e18f321ef0da754
Kieferer <balazsborsi0@gmail.com>
2023-07-08 20:37:19 +0200

    feat: Add useEffect and a/setA to test storing and calling of get_file_system_representation method

--- src/elements/window/tool/hierarchy/Hierarchy.jsx
+++ src/elements/window/tool/hierarchy/Hierarchy.jsx
@@ -1,6 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {invoke} from '@tauri-apps/api/tauri';
 import './hierarchy.css'
 const Hierarchy = () => {
+  const [a, setA] = useState();
+  useEffect(() => {
+    invoke("get_file_system_representation", {origin: "D:\\asd"}).then(x => console.log(x));
+  }, [])
+  
+
   return (
     <div className={"hierarchyPanel"} id="myResizableDiv">
       <p className={"hierarchyTitle"}>HIERARCHY</p>
