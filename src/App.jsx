2042e8f2ca2ec119198d982a090606f8aab512e4
Kieferer <balazsborsi0@gmail.com>
2023-07-13 21:52:21 +0200

    feat: Add shared variable between Hierarchy and CodeEditor to Hierarchy be able to change the displayed text on CodeEditor if its loaded a file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import TitleBar from "./elements/window/title/TitleBar.jsx";
 import Hierarchy from "./elements/window/tool/hierarchy/Hierarchy.jsx";
 import CodeEditor from "./elements/window/tool/codeEditor/CodeEditor.jsx";
+import { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState('');
   return (
     <div className="container">
       <TitleBar />
-      <Hierarchy/>
-      <CodeEditor/>
+      <Hierarchy updatedCode={setCode}/>
+      <CodeEditor updatedCode={code}/>
     </div>
   );
 }
