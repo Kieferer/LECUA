e038bd5de93f8fb2863c3422d1995d6d0f280fa6
Kieferer <balazsborsi0@gmail.com>
2023-07-13 21:56:23 +0200

    feat: Add path and loadFile arguments, implement child Folder and File creation

--- src/elements/window/tool/hierarchy/Folder.jsx
+++ src/elements/window/tool/hierarchy/Folder.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import File from './File';
 
-function Folder({ name, children }) {
+function Folder({ name, path, children, loadFile }) {
   const [visable, setVisable] = useState(true);
   const handleClick = () => {
     setVisable(!visable);
@@ -16,8 +16,9 @@ function Folder({ name, children }) {
         {name}
       </div>
       {visable && <ul>
-        {children && children.map((element, id) => element.children ? <Folder key={id} name={element.name} children={element.children} /> :
-          <File key={id} name={element.name} />)}
+        {children && children.map((element, id) => element.children ?
+          <Folder key={id} name={element.name} children={element.children} path={element.path} loadFile={loadFile} /> :
+          <File key={id} name={element.name} path={element.path} loadFile={loadFile} />)}
       </ul>
       }
     </div>
