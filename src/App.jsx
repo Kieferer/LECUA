4b431273736b3e8da61fb3c16d93f5868b174781
Kieferer <balazsborsi0@gmail.com>
2023-08-07 21:16:08 +0200

    feat: Add outputLog variable and Terminal element to App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import "./App.css";
 import TitleBar from "./elements/window/title/TitleBar.jsx";
 import Hierarchy from "./elements/window/tool/hierarchy/Hierarchy.jsx";
 import CodeEditor from "./elements/window/tool/codeEditor/CodeEditor.jsx";
-import { useState } from "react";
+import Terminal from "./elements/window/tool/terminal/Terminal";
+import { useState, useEffect } from "react";
 
 function App() {
   const [code, setCode] = useState('');
   const [filePath, setFilePath] = useState('');
+  const [outputLog, setOutputLog] = useState("");
+
   return (
     <div className="container">
-      <TitleBar />
+      <TitleBar/>
       <Hierarchy updatedCode={setCode} filePath={filePath} setFilePath={setFilePath}/>
-      <CodeEditor updatedCode={code} filePath={filePath} setFilePath={setFilePath}/>
+      <CodeEditor updatedCode={code} setOutputLog={setOutputLog} filePath={filePath} setFilePath={setFilePath}/>
+      <Terminal output={outputLog} setOutputLog={setOutputLog}/>
     </div>
   );
 }
