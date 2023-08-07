b361f7bfcb5fe4ad5129f347e8f0a4d7437fe2a6
Kieferer <balazsborsi0@gmail.com>
2023-08-07 21:12:37 +0200

    refactor: Remove unused arguments and validation from handleKeyEnter method. Remove event register useEffect.

--- src/elements/window/tool/hierarchy/Hierarchy.jsx
+++ src/elements/window/tool/hierarchy/Hierarchy.jsx
@@ -1,50 +1,43 @@
 import { useEffect, useState, useRef } from "react";
-import {invoke} from '@tauri-apps/api/tauri';
+import { invoke } from '@tauri-apps/api/tauri';
 import Folder from "./Folder";
 import './hierarchy.css'
 
-const Hierarchy = ({updatedCode, filePath, setFilePath, lastSelectedElement}) => {
+const Hierarchy = ({ updatedCode, filePath, setFilePath }) => {
   const [data, setData] = useState();
   const [url, setURL] = useState("");
   const urlInputRef = useRef(null);
-  
+
   const loadRepository = (root) => {
-    invoke("get_file_system_representation", {origin: root}).then(x => setData(JSON.parse(x)));
+    invoke("get_file_system_representation", { origin: root }).then(x => setData(JSON.parse(x)));
   }
 
   const loadFile = (path) => {
     setFilePath(path);
-    invoke("load_file", {path: path}).then(data => updatedCode(data));
+    invoke("load_file", { path: path }).then(data => updatedCode(data));
   }
 
   const handleKeyEnter = (event) => {
-    console.log("asd");
-    if (event.target.id === urlInputRef.current.id) {
-      if (event.key === "Enter") {
-        setURL(urlInputRef.current.value);
-        loadRepository(url);
-      }
+    if (event.key === "Enter") {
+      setURL(urlInputRef.current.value);
+      loadRepository(urlInputRef.current.value);
     }
   }
-
-  useEffect(() => {
-    invoke("get_running_location").then(x => setURL(x)).then(urlInputRef.current.value = url);
-    urlInputRef.current.value = url;
-    url&&loadRepository(url);
-  }, [url])
   
   useEffect(() => {
-    document.addEventListener('keydown', console.log("asd"));
-    return () => {
-      document.removeEventListener('keydown', console.log("asd"));
-    }
-  }, [])
+    invoke("get_running_location").then(location => {
+      setURL(location);
+      urlInputRef.current.value = location;
+      loadRepository(location);
+    });
+  }, []);
+  
 
   return (
     <div className={"hierarchyPanel"} id="hierarchy">
-      <input ref={urlInputRef} id="hierarchy" className="urlInput" type="text" onChange={handleKeyEnter} placeholder="C:\ ..."/>
+      <input ref={urlInputRef} id="hierarchy" className="urlInput" onKeyDown={handleKeyEnter} type="text" />
       <div className="folderContent">
-        {data && <Folder id="hierarchy" name={data.name} children={data.children} loadFile={loadFile}/>}
+        {data && <Folder name={data.name} children={data.children} loadFile={loadFile} />}
       </div>
     </div>
   )
