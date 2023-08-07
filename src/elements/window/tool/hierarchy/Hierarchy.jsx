a268baa08be067b5bb8ee8bbe4dff723b0e1793a
Kieferer <balazsborsi0@gmail.com>
2023-08-07 20:09:55 +0200

    feat: Add url and urlInputRef variables. Encapsulate folders into folderContent div for css. Implement handleKeyEnter method and add event register and file loader useEffect

--- src/elements/window/tool/hierarchy/Hierarchy.jsx
+++ src/elements/window/tool/hierarchy/Hierarchy.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import {invoke} from '@tauri-apps/api/tauri';
 import Folder from "./Folder";
 import './hierarchy.css'
 
-const Hierarchy = ({updatedCode, filePath, setFilePath}) => {
+const Hierarchy = ({updatedCode, filePath, setFilePath, lastSelectedElement}) => {
   const [data, setData] = useState();
+  const [url, setURL] = useState("");
+  const urlInputRef = useRef(null);
   
   const loadRepository = (root) => {
     invoke("get_file_system_representation", {origin: root}).then(x => setData(JSON.parse(x)));
@@ -15,9 +17,35 @@ const Hierarchy = ({updatedCode, filePath, setFilePath}) => {
     invoke("load_file", {path: path}).then(data => updatedCode(data));
   }
 
+  const handleKeyEnter = (event) => {
+    console.log("asd");
+    if (event.target.id === urlInputRef.current.id) {
+      if (event.key === "Enter") {
+        setURL(urlInputRef.current.value);
+        loadRepository(url);
+      }
+    }
+  }
+
+  useEffect(() => {
+    invoke("get_running_location").then(x => setURL(x)).then(urlInputRef.current.value = url);
+    urlInputRef.current.value = url;
+    url&&loadRepository(url);
+  }, [url])
+  
+  useEffect(() => {
+    document.addEventListener('keydown', console.log("asd"));
+    return () => {
+      document.removeEventListener('keydown', console.log("asd"));
+    }
+  }, [])
+
   return (
-    <div className={"hierarchyPanel"}>
-      {data && <Folder name={data.name} children={data.children} loadFile={loadFile}/>}
+    <div className={"hierarchyPanel"} id="hierarchy">
+      <input ref={urlInputRef} id="hierarchy" className="urlInput" type="text" onChange={handleKeyEnter} placeholder="C:\ ..."/>
+      <div className="folderContent">
+        {data && <Folder id="hierarchy" name={data.name} children={data.children} loadFile={loadFile}/>}
+      </div>
     </div>
   )
 };
