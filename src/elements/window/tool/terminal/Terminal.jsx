08604ff7b5cb51aaa97a8c5144e3f2d216176c5f
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-08-09 13:45:41 +0200

    chore: Move from custom input field to contentEditable <div/>. Remove and modify custom input specific methods.

--- src/elements/window/tool/terminal/Terminal.jsx
+++ src/elements/window/tool/terminal/Terminal.jsx
@@ -1,57 +1,74 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import './terminal.css';
 
 function Terminal({ output, setOutputLog }) {
-  const [terminalInput, setTerminalInput] = useState("");
+  const [commandInput, setCommandInput] = useState('');
+  const contentRef = useRef(null);
   const terminalRef = useRef(null);
 
   const scrollToBottom = () => {
     terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
-  }
-
-  const sendCommand = (input) => {
-    invoke("send_command_to_terminal", { command: input }).then(out => setOutputLog(output + "\n" + out));
-  }
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Backspace") {
-      if (terminalInput.length > 0) {
-        setOutputLog(prevText => prevText.slice(0, -1));
-        setTerminalInput(prevText => prevText.slice(0, -1));
-      }
-    }
-    if (event.key === "Enter") {
-      sendCommand(terminalInput);
-      setTerminalInput("");
-    }
-    if (event.key.length < 2) {
-      event.preventDefault();
-      setTerminalInput(prevInput => prevInput + event.key);
-      setOutputLog(prevLog => prevLog + event.key);
-    }
   };
 
-  useEffect(() => {
-    invoke("send_command_to_terminal", { command: "" }).then(out => setOutputLog(out));
-  }, [])
+  const sendCommand = (input) => {
+    invoke('send_command_to_terminal', { command: input }).then((out) =>
+      setOutputLog((prevOutput) => prevOutput + '\n' + out)
+    );
+  };
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === 'Enter') {
+        console.log(event.key)
+        event.preventDefault();
+        if (commandInput == "clear"){
+          setOutputLog("")
+          contentRef.current.innerHTML = "";
+        }
+        sendCommand(commandInput);
+        setCommandInput("");
+      }
+      if (event.key === 'Backspace') {
+        if (commandInput.length <= 0) {
+          event.preventDefault();
+        } else {
+          setCommandInput((text) => text.substring(0, text.length - 1));
+        }
+      }
+      if (event.key.length < 2) {
+        setCommandInput((text) => text + event.key);
+      }
+    },
+    [commandInput]
+  );
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
+    invoke('send_command_to_terminal', { command: '' }).then((out) => {
+      setOutputLog(out);
+      contentRef.current.value = out;
+    });
   }, []);
 
   useEffect(() => {
+    contentRef.current.addEventListener('keydown', handleKeyDown);
+    return () => {
+      contentRef.current.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  useEffect(() => {
+    contentRef.current.innerHTML = output;
     scrollToBottom();
   }, [output]);
 
   return (
-    <div className='terminal' ref={terminalRef} id={'terminal'}>
-      <pre>{output}</pre>
+    <div className='terminal' ref={terminalRef}>
+      <pre>
+        <div className='content' ref={contentRef} id={'terminal'} contentEditable></div>
+      </pre>
     </div>
   );
 }
 
-export default Terminal
+export default Terminal;
