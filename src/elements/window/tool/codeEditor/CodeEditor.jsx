004bb212a8d5c8a0bafb1f3969b3e8e04a78efe2
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-08-09 13:50:31 +0200

    chore: Replace custom input field with contentEditable <div/>. Simplify shortcut methods, Remove unused imports

--- src/elements/window/tool/codeEditor/CodeEditor.jsx
+++ src/elements/window/tool/codeEditor/CodeEditor.jsx
@@ -5,51 +5,15 @@ import 'prismjs/components/prism-javascript';
 import { invoke } from '@tauri-apps/api/tauri';
 import './codeEditor.css'
 
-const CodeEditor = ({ updatedCode, filePath, setFilePath, setOutputLog }) => {
-  const [cursorPos, setCursorPos] = useState(0);
-  const [cursorPosX, setCursorPosX] = useState(0);
-  const [cursorPosY, setCursorPosY] = useState(0);
+const CodeEditor = ({ updatedCode, setOutputLog }) => {
   const [code, setCode] = useState('');
-  const [visualizerCode, setVisualizerCode] = useState('')
   const editorRef = useRef(null);
 
   const handleKeyDown = (event) => {
-    if (event.key === "ArrowRight") {
-      setCursorPos((value) => Math.min(code.length, value + 1));
-      /*invoke("adjust_cursor_x", {text: code, x: cursorPosX, y: cursorPosY, direction: 1}).then(([x, y]) => {
-        setCursorPosX(x);
-        setCursorPosY(y);
-      });*/
-    }
-    if (event.key === "ArrowLeft") {
-      setCursorPos((value) => Math.max(0, value - 1));
-      //invoke("adjust_cursor_x", {text: code, x: cursorPosX, y: cursorPosY, direction: -1})
-    }
-    if (event.key === "ArrowUp") {
-      //invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: -1}).then((value) => setCursorPosY(value));
-      //invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPos((value) => value - y_length));
-    }
-    if (event.key === "ArrowDown") {
-      //invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: 1}).then((value) => setCursorPosY(value));
-      //invoke('get_length_of_lines', {text: code, y: cursorPosY}).then(y_length => setCursorPos((value) => value + y_length));
-    }
-    if (event.key === "Backspace") {
-      setCode((prevText) => prevText.slice(0, -1));
-      setCursorPos((value) => Math.max(0, value - 1));
-      setCursorPosY((value) => Math.min(code.length, value + 1));
-      return;
-    }
-
-    if (event.key === "Enter") {
-      setCode((prevText) => prevText + "\n");
-      setCursorPos((value) => value + 1);
-      return;
-    }
-
+    Prism.highlightAll();
     if (event.ctrlKey) {
       if (event.key === "t") {
         invoke("compile", { code: code }).then(output => setOutputLog(output));
-        //invoke("adjust_cursor_y", {text: code, y: cursorPosY, direction: 0}).then(m => console.log(m))
       }
       return;
     }
@@ -71,11 +35,6 @@ const CodeEditor = ({ updatedCode, filePath, setFilePath, setOutputLog }) => {
           return;
       }
     }
-    if (event.key.length < 2) {
-      event.preventDefault();
-      setCode((prevText) => prevText + event.key);
-      setCursorPos((value) => value + 1);
-    }
   };
 
   useEffect(() => {
@@ -85,12 +44,16 @@ const CodeEditor = ({ updatedCode, filePath, setFilePath, setOutputLog }) => {
   }, [updatedCode]);
 
   useEffect(() => {
+    editorRef.current.innerHTML = code;
+  }, [code]);
+
+  /*useEffect(() => {
     invoke('insert_cursor_symbol', { text: code, pos: Math.min(code.length, cursorPos) }).then((message => {
       setVisualizerCode(message);
     }));
     setCursorPos((value) => Math.min(code.length, value));
-    setTimeout(Prism.highlightAll, 1);
-  }, [visualizerCode, cursorPos, code]);
+    Prism.highlightAll();
+  }, [visualizerCode, cursorPos, code]);*/
 
   useEffect(() => {
     editorRef.current.addEventListener('keydown', handleKeyDown);
@@ -100,13 +63,12 @@ const CodeEditor = ({ updatedCode, filePath, setFilePath, setOutputLog }) => {
   }, []);
 
   return (
-    <div className="editorPanel" ref={editorRef} id={"editor"}>
+    <div className="editorPanel">
       <pre className="language-javascript">
-        <code className="language-javascript">
-          {visualizerCode}
-        </code>
+        <code className="language-javascript" ref={editorRef} id={"editor"} contentEditable>{}</code>
       </pre>
     </div>
   )
-};
+}
+
 export default CodeEditor;
