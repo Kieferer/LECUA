bfd25f78e2518e0b629ed6f4a5862afba3c4865a
Kieferer <balazsborsi0@gmail.com>
2023-08-10 22:09:44 +0200

    feat: Implement CodeMirror and remove PrismJS specific code. Set compile shortcut to 'Alt+T' as testing

--- src/elements/window/tool/codeEditor/CodeEditor.jsx
+++ src/elements/window/tool/codeEditor/CodeEditor.jsx
@@ -1,36 +1,23 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
-import Prism from 'prismjs';
-import 'prismjs/components/prism-rust.js';
 import './codeEditor.css'
-import './code.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { createTheme } from '@uiw/codemirror-themes';
+import { rust } from '@codemirror/lang-rust';
+import { tags } from '@lezer/highlight';
 
 const CodeEditor = ({ updatedCode, setOutputLog }) => {
   const [code, setCode] = useState('');
   const editorRef = useRef(null);
 
-  const handleKeyDown = useCallback((event) => {
-    if (event.ctrlKey) {
-      if (event.key == "h"){
-        Prism.highlightAll();
-      }
-      if (event.key === "t") {
-        invoke("compile", { code: code }).then(output => setOutputLog(output));
-      }
-      return;
-    }
-    if (event.altKey) {
-      if (event.key == "c"){
-        if (editorRef.current.includes("main.")) {
-          let codeWithoutSpreadKeyWord = code.replace("main.", "");
-          editorRef.current.value = codeWithoutSpreadKeyWord +
-            "pub fn main() {\n" +
-            "    println!(\"Hello, World!\");\n" +
-            "}";
-        }
+  const handleKeyDown = (event) => {
+    if (event.altKey){
+      const key = event.code;
+      switch (key) {
+        case "KeyT": invoke("compile", { code: code }).then(output => setOutputLog(output)); break;
       }
     }
-  })
+  }
 
   useEffect(() => {
     if (updatedCode) {
@@ -39,22 +26,53 @@ const CodeEditor = ({ updatedCode, setOutputLog }) => {
   }, [updatedCode]);
 
   useEffect(() => {
-    editorRef.current.innerHTML = code;
-    Prism.highlightAll();
+    editorRef.current&&editorRef.current.editor.addEventListener('keydown', handleKeyDown);
+    return () => {
+      editorRef.current&&editorRef.current.editor.removeEventListener('keydown', handleKeyDown);
+    };
   }, [code]);
 
-  useEffect(() => {
-    editorRef.current.addEventListener('keydown', handleKeyDown);
-    return () => {
-      editorRef.current.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+  const rustTheme = createTheme({
+    settings: {
+      background: '#181818',
+      foreground: '#E5E5E5',
+      caret: '#FFFFFF',
+      selection: '#0F236C',
+      selectionMatch: '#333333',
+      lineHighlight: '#0F0F0F',
+      gutterBackground: '#262626',
+      gutterForeground: '#8a919966',
+      gutterBorder: '#181818',
+    },
+    styles: [
+      { tag: tags.comment, color: '#6A737D' },
+      { tag: tags.variableName, color: '#5A9BD5' },
+      { tag: [tags.string, tags.special(tags.brace)], color: '#6D877B' },
+      { tag: tags.number, color: '#B0856E' },
+      { tag: tags.bool, color: '#B0856E' },
+      { tag: tags.null, color: '#B0856E' },
+      { tag: tags.keyword, color: '#C678DD' },
+      { tag: tags.operator, color: '#D4D7D6' },
+      { tag: tags.className, color: '#E5C07B' },
+      { tag: tags.definition(tags.typeName), color: '#E06C75' },
+      { tag: tags.typeName, color: '#E5C07B' },
+      { tag: tags.angleBracket, color: '#D4D7D6' },
+      { tag: tags.tagName, color: '#D4D7D6' },
+      { tag: tags.attributeName, color: '#D4D7D6' },
+      { tag: tags.operatorKeyword, color: '#61AFEF' }
+    ],
+  });
 
   return (
     <div className="editorPanel">
-      <pre className="language-rust" tabIndex={-1}>
-        <code className='content' ref={editorRef} id={"editor"} contentEditable spellCheck="false">{ }</code>
-      </pre>
+      <CodeMirror
+        ref={editorRef}
+        value={code}
+        theme={rustTheme}
+        extensions={[rust({})]}
+        spellCheck='false'
+        onChange={(current) => setCode(current)}
+      />
     </div>
   )
 }
