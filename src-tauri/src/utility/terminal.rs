8430304a29e35b51a37d7dd89c9b6e6a7a2c2c56
Kieferer <balazsborsi0@gmail.com>
2023-08-18 21:10:30 +0200

    feat: Create send_command_to_session method, a version of send_command_to_terminal which use session

--- src-tauri/src/utility/terminal.rs
+++ src-tauri/src/utility/terminal.rs
@@ -31,3 +31,27 @@ pub fn start_persistent_session() -> String {
 
     job_id.to_string()
 }
+#[tauri::command]
+pub fn send_command_to_session(command: &str) -> String {
+    let output = Command::new("powershell")
+        .args(&[
+            "-NoLogo",
+            "-NoProfile",
+            "-Command",
+            &format!("Invoke-Command -Session ${{Enter-PSSession -Id {}}} -ScriptBlock {{ {}}} | Out-String", 0, command)
+        ])
+        .output()
+        .expect("Failed to send command to session");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    let mut result = String::new();
+    if !stderr.is_empty() {
+        result = "\nError Output:\n".parse().unwrap();
+        result += &stderr;
+    }
+    result += &*stdout.to_string();
+
+    result
+}
