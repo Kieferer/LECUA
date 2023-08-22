31a98260997b5091d9186e406a39a46a732abd7c
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-08-22 14:06:09 +0200

    feat: Create fileMenuContent, editMenuContent and viewMenu content. Assign functions to Button components. Add Menu component to container

--- src/elements/window/title/TitleBar.jsx
+++ src/elements/window/title/TitleBar.jsx
@@ -1,14 +1,41 @@
 import { appWindow } from '@tauri-apps/api/window';
-import React, { useEffect } from 'react';
-import './titleBar.css';
+import React, { useEffect, useState } from 'react';
 import Button from "../../button/Button.jsx";
 import NavigationMenu from "./navigationMenu/NavigationMenu.jsx";
+import Menu from './menu/Menu';
+import './titleBar.css';
 
 const TitleBar = () => {
+  const [menuContent, setMenuContent] = useState(null);
+
   useEffect(() => {
     const titlebar = document.getElementById("titlebar");
     titlebar.addEventListener("drag", appWindow.startDragging)
   }, [])
+
+  const fileMenuContent = [
+    {label: "Open file", xPos: "45px"},
+    {label: "Open folder", xPos: "45px"},
+    {label: "Open workspace", xPos: "45px"},
+    {label: "Save file", xPos: "45px"},
+    {label: "Exit", action: () => appWindow.close(), xPos: "45px"}
+  ];
+  const editMenuContent = [
+    {label: "Undo", xPos: "125px"},
+    {label: "Redo", xPos: "125px"},
+    {label: "Cut", xPos: "125px"},
+    {label: "Copy", xPos: "125px"},
+    {label: "Paste", xPos: "125px"},
+    {label: "Find", xPos: "125px"},
+    {label: "Replace", xPos: "125px"}
+  ];
+  const viewMenuContent = [
+    {label: "Terminal", xPos: "200px"},
+    {label: "Hierarchy", xPos: "200px"},
+    {label: "CodeEditor", xPos: "200px"},
+    {label: "Zoom in", xPos: "200px"},
+    {label: "Zoom out", xPos: "200px"}
+  ];
 
   return (
     <div data-tauri-drag-region className="titlebar" id={"titlebar"}>
@@ -17,13 +44,14 @@ const TitleBar = () => {
           <img src="/lecuaLogo.png" className="logoApp" alt="Vite logo" />
         </div>
         <div className="titlebar-menu">
-            <Button label={"File"}/>
-            <Button label={"Edit"}/>
-            <Button label={"View"}/>
+          <Button label={"File"} action={() => setMenuContent(fileMenuContent)}/>
+          <Button label={"Edit"} action={() => setMenuContent(editMenuContent)}/>
+          <Button label={"View"} action={() => setMenuContent(viewMenuContent)}/>
+          <Menu content={menuContent} setContent={setMenuContent}/>
         </div>
       </div>
       <div className={"titleName"}>LECUA</div>
-      <NavigationMenu/>
+      <NavigationMenu />
     </div>
   );
 };
